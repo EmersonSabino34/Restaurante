4850197a92954252d47a88d32177571f1c8d44cb
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-09-04 22:44:24 +0100

    Atualizar o script.js

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -8,7 +8,6 @@ $(document).ready(function() {
     const sections = $('sections');
     const navItems = $('.nav-item');
 
-
     $(window).on('scroll', function() {
         const header = $('header');
         const scrollPosition = $(window).scrollTop() - header.outerHeight();
